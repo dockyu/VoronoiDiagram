e70f14ce27ea9f17a8307f21fe566896bf8f4e3f
HelloWorld <dockyu.chen@gmail.com>
2023-11-17 16:20:40 +0800

    Update module-info.java
    In the latest update of the JavaFX application, the packages datastruct, algorithm, and voronoidiagram are now hidden from external access.

--- source/src/main/java/module-info.java
+++ source/src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module dev.dockyu.voronoidiagram {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
-
-    opens dev.dockyu.voronoidiagram to javafx.fxml;
-    opens dev.dockyu.voronoidiagram.datastruct to javafx.fxml;
-    opens dev.dockyu.voronoidiagram.algorithm to javafx.fxml;
-    exports dev.dockyu.voronoidiagram;
-    exports dev.dockyu.voronoidiagram.datastruct;
-    exports dev.dockyu.voronoidiagram.algorithm;
+    // 為JavaFX開放主應用程序類所在的包
+    opens dev.dockyu.voronoidiagram to javafx.graphics, javafx.fxml;
+//    exports dev.dockyu.voronoidiagram;
+//    exports dev.dockyu.voronoidiagram.datastruct;
+//    exports dev.dockyu.voronoidiagram.algorithm;
 }
